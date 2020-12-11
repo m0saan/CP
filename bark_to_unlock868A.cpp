5ae90ae15093f28599b2f5aec9a08320f0bca07a
MoBoustta <moboustta6@gmail.com>
2020-12-11 16:27:57 +0100

    coding the solution of 868A problem

--- bark_to_unlock868A.cpp
+++ bark_to_unlock868A.cpp
@@ -1,4 +1,18 @@
-//
-// Created by moboustt on 11/12/2020.
-//
-
+#include <iostream>
+using namespace std;
+int main() {
+    int n, ans{};
+    string passWord, SS;
+    cin >> passWord >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> SS;
+        if (SS[0] == passWord.at(1)) ans++;
+        if (SS[1] == passWord.at(0)) ans++;
+        if (ans == 2 || SS == passWord){
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+    cout << "NO" << endl;
+    return 0;
+}
