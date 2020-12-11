66c1c5617944a6d4cafb75e8b50ba1b73872cedb
MoBoustta <moboustta6@gmail.com>
2020-12-11 16:46:11 +0100

    codeforces 868A solution

--- bark_to_unlock868A.cpp
+++ bark_to_unlock868A.cpp
@@ -4,10 +4,17 @@ int main() {
     int n, ans{};
     string passWord, SS;
     cin >> passWord >> n;
+	bool c1 = false, c2 = false;
     for (int i = 0; i < n; ++i) {
         cin >> SS;
-        if (SS[0] == passWord.at(1)) ans++;
-        if (SS[1] == passWord.at(0)) ans++;
+        if (!c1 && SS[0] == passWord.at(1)){
+		   	ans++;
+			c1 = true;
+		}
+        if (!c2 && SS[1] == passWord.at(0)){ 
+			ans++;
+			c2 = true;
+		}
         if (ans == 2 || SS == passWord){
             cout << "YES" << endl;
             return 0;
