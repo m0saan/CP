75fb3482fd943c96b851d02b104a5d1b587a683e
MoBoustta <moboustta6@gmail.com>
2020-12-11 21:03:20 +0100

    /*/

--- bits_manipulation.cpp
+++ bits_manipulation.cpp
@@ -1,2 +1,17 @@
-
+#include <iostream>
+using namespace std:
+int main(){
+	int n, in;
+	cin >> n;
+	vector<int> v(n);
+	for (size_t i=0; i<n; ++i){
+		cin >> v[i];
+		bool ans = false;
+		for (size_t j=1; j <= ; ++j)
+			if (i == v[v[v[i]]]) ans = true;
+	}
+	if (ans) cout << "YES" << endl;
+	else cout << "NO" << endl;
+	return 0;
+}
 
