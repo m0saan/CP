34c1435d6036ce37cf87b7d5609552476117f484
MoBoustta <moboustta6@gmail.com>
2020-12-12 12:11:45 +0100

    939A problem set solution -codeforces- not yet workin

--- 939A.cpp
+++ 939A.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+int n,v[5001];
 int main(){
-	int n;
 	cin >> n;
-	vector<int> v(n);
-    bool ans = false;
+    bool ans;
 	for (size_t i=0; i<n; ++i) {
+
         cin >> v[i];
         ans = false;
         for (size_t j = 1; j <= i; ++j) {
@@ -15,6 +15,7 @@ int main(){
                 break;
             }
         }
+        if (ans) break;
     }
 	if (ans) cout << "YES" << endl;
 	else cout << "NO" << endl;
