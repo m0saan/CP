92c5906569c571e16c11abf1374479bf7c18fdc8
MoBoustta <moboustta6@gmail.com>
2020-12-12 16:02:46 +0100

    start coding 263A solution

--- 263A.cpp
+++ 263A.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
-int len = 25, a[5][5];
+#include <vector>
 using namespace std;
 int main(){
+    vector <vector<int>> a(25)
+    int P{};
     for (size_t i=0; i<5; ++i){
-        for (size_t i=0; i<5; ++i)
-            cin >> a[i];
+        for (size_t j=0; j<5; ++j){
+            cin >> a[j];
+            if (a[j] != 1) ++P;
+        }
     }
+    cout << abs(13 - P);
     return 0;
 }
