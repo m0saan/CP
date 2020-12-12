2adbff12690408537b38742dfc6bf444e4046040
MoBoustta <moboustta6@gmail.com>
2020-12-12 15:48:31 +0100

    start coding 263A solution

---
+++ 263A.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int len = 25, a[5][5];
+using namespace std;
+int main(){
+    for (size_t i=0; i<5; ++i){
+        for (size_t i=0; i<5; ++i)
+            cin >> a[i];
+    }
+    return 0;
+}
