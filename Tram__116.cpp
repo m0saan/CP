71ef2799d404d76261f3e4bb6de8096ea4494853
MoBoustta <moboustta6@gmail.com>
2020-12-15 20:20:17 +0100

    116A problem set solution -codeforces-

--- Tram__116.cpp
+++ Tram__116.cpp
@@ -1,4 +1,14 @@
-//
-// Created by Mohammad  Boustta on 12/15/20.
-//
-
+#include <iostream>
+using namespace std;
+int main(){
+    int n, in1, in2, ans{}, cpt{};
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> in1 >> in2;
+        ans -= in1;
+        ans += in2;
+        cpt = max(cpt, ans);
+    }
+    cout << cpt << endl;
+    return 0;
+}
