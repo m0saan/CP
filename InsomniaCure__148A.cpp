8dd96dd9a57158ecfd37e95f007ec96f927aad32
MoBoustta <moboustta6@gmail.com>
2020-12-15 13:48:16 +0100

    136A problem set solution -codeforces-

---
+++ InsomniaCure__148A.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int k, l, m, n, d;
+	cin >> k >> l >> m >> n >> d;
+
+	return 0;
+}
--- Presents__136A.cpp
+++ Presents__136A.cpp
@@ -1,4 +1,16 @@
-//
-// Created by Mohammad  Boustta on 12/15/20.
-//
-
+#include <iostream>
+#include <map>
+using namespace std;
+int main(){
+    int n, in;
+    map<int,int> m;
+    cin >> n;
+    for (int i=1; i <= n; ++i){
+        cin >> in;
+        m[in] = i;
+    }
+    for (auto &p : m)
+        cout << p.second << ' ';
+    cout << endl;
+    return 0;
+}
