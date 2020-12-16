6be1a93fa93db92dda46e8a96e5b59e3b15c1365
MoBoustta <moboustta6@gmail.com>
2020-12-16 10:30:51 +0100

    92A problem set solution -codeforces-

--- Chips__92A.cpp
+++ Chips__92A.cpp
@@ -1,4 +1,20 @@
-//
-// Created by Mohammad  Boustta on 12/16/20.
-//
-
+#include <iostream>
+using namespace std;
+int main(){
+    int n,m, need;
+    cin >> n >> m;
+    if (n==1){
+        cout << 0 << endl;
+        return 0;
+    }
+    for (int i = 1; ; ++i) {
+        if (i == n) need  = (i%(n+1));
+        else need  = ((i)%n);
+        if (need > m) {
+            cout << m << endl;
+            return 0;
+        }
+        if (i>n) i=1;
+        m-=need;
+    }
+}
