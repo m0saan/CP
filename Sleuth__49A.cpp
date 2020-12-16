8375e29562163b8738dbbc87670b8db9a0c8237b
MoBoustta <moboustta6@gmail.com>
2020-12-16 20:27:04 +0100

    49A problem set solution -codeforces-

--- Sleuth__49A.cpp
+++ Sleuth__49A.cpp
@@ -1,4 +1,18 @@
-//
-// Created by Mohammad  Boustta on 12/16/20.
-//
-
+#include <iostream>
+#include <set>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+int main(){
+    set<int> s{'A', 'E', 'I', 'O', 'U', 'Y'};
+    string S;
+    cin >> S;
+    istringstream iss(S);
+    vector<string> results(std::istream_iterator<std::string>{iss},
+                                     std::istream_iterator<std::string>());
+    string T = results[results.size()-2];
+    if (s.count(toupper(T.at(T.size()-2)))) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    return 0;
+}
