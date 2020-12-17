6bb4fe707dbcfef0147b695a8cb5ac54526b6588
MoBoustta <moboustta6@gmail.com>
2020-12-17 17:44:14 +0100

    38A problem set solution -codeforces-

---
+++ Army__38A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n, in, ans{};
+    cin >> n;
+    vector<int> v1(n-1);
+    vector<int> v2(n-1);
+    for (int i = 0; i < n-1; ++i) cin >> v1[i];
+    for (int i = 0; i < 2; ++i) cin >> v2[i];
+    for (int i = v2[0]-1; i < v2[1]-1; ++i)
+        ans += v1[i];
+    cout <<  ans << endl;
+    return 0;
+}
