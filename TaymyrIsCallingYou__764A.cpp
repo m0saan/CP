0bd06252b5f8fcb50280defe30305ca57061c8ee
MoBoustta <moboustta6@gmail.com>
2020-12-18 15:03:33 +0100

    764A problem set solution -codeforces-

--- TaymyrIsCallingYou__764A.cpp
+++ TaymyrIsCallingYou__764A.cpp
@@ -1,4 +1,11 @@
-//
-// Created by Mohammad  Boustta on 12/18/20.
-//
-
+#include <iostream>
+#include <numeric>
+using namespace std;
+int main(){
+    int n,m,z, ans{};
+    cin >> n >> m >> z;
+    int g = gcd(m,n);
+    int l = m*n / g;
+    cout << z /l << endl;
+    return 0;
+}
