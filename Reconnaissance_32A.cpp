e21feea05c071e2fa566825b1ea87690137403d2
MoBoustta <moboustta6@gmail.com>
2020-12-18 12:42:25 +0100

    32A problem set solution -codeforces-

--- Reconnaissance_32A.cpp
+++ Reconnaissance_32A.cpp
@@ -1,4 +1,13 @@
-//
-// Created by Mohammad  Boustta on 12/17/20.
-//
+#include <iostream>
+#include <numeric>
+using namespace std;
 
+int main(){
+    int Y,W, ans{};
+    cin >> Y >> W;
+    for (int i = 1; i <=6 ; ++i)
+        if (i != Y || i!=W) ans++;
+    int g = gcd(Y,W);
+    cout << ans / g << '/' << 6 / g << endl;
+    return 0;
+}
