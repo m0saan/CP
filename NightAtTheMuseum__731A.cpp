122febbb2767246eb6e82fbdedc1d4a331cb9c5e
MoBoustta <moboustta6@gmail.com>
2020-12-18 21:03:21 +0100

    731A problem set solution -codeforces-

--- NightAtTheMuseum__731A.cpp
+++ NightAtTheMuseum__731A.cpp
@@ -1,4 +1,18 @@
-//
-// Created by Mohammad  Boustta on 12/18/20.
-//
-
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    string s;
+    cin >> s;
+    char ch = 1;
+    int ans{};
+    // a:97, z:122
+    // min(|x-y|,l-|x-y|)
+    for (char &c:s){
+        int ll = (int)c - 96;
+        ans += min(abs(ch-ll), 26-abs(ch-ll));
+        ch = (int)c - 96;
+    }
+    cout << ans << endl;
+    return 0;
+}
