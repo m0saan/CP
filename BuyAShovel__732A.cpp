70d7d6045a1bf3900074b08b695b0744b141f2a3
MoBoustta <moboustta6@gmail.com>
2020-12-18 21:03:03 +0100

    732A problem set solution -codeforces-

---
+++ BuyAShovel__732A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int k,r;
+    cin >> k >> r;
+    for (int i=1;i<1000;i++){
+        if (k*i % 10==0 || k*i % 10==r) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
