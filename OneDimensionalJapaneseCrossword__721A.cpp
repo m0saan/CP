f05d8611aaa60f8044ccbc5d98026b6826d024b0
MoBoustta <moboustta6@gmail.com>
2020-12-19 11:41:19 +0100

    721A problem set solution -codeforces-

--- OneDimensionalJapaneseCrossword__721A.cpp
+++ OneDimensionalJapaneseCrossword__721A.cpp
@@ -1,4 +1,22 @@
-//
-// Created by Mohammad  Boustta on 12/19/20.
-//
-
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int n, count{}, ans{}, i{};
+    vector<int> v;
+    string S;
+    cin >> n >> S;
+    for (char c : S){
+        if (c == 'B') count++;
+        else if (count){
+            i++;
+            v.push_back(count);
+            count=0;
+        }
+    }
+    if (count) {v.push_back(count); ++i;}
+    cout << i << endl;
+    for (int e : v)
+        cout << e << ' ';
+    return 0;
+}
