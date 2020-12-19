d1fed266e0378c3d71ecf40cdff20336a1de692e
MoBoustta <moboustta6@gmail.com>
2020-12-19 17:48:46 +0100

    703A problem set solution -codeforces-

--- MishkaAndGame__703A.cpp
+++ MishkaAndGame__703A.cpp
@@ -1,4 +1,15 @@
-//
-// Created by Mohammad  Boustta on 12/19/20.
-//
-
+#include <iostream>
+using namespace std;
+int main(){
+    int n,a,b,M{}, C{};
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> a >> b;
+        if (a>b)++M;
+        else if (a<b) ++C;
+    }
+    if (M>C) cout << "Mishka" << endl;
+    else if (M<C) cout << "Chris" << endl;
+    else cout << "Friendship is magic!^^" << endl;
+    return 0;
+}
