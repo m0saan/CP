b9293eb7979db24830121aa38c07773f8f1e7d09
MoBoustta <moboustta6@gmail.com>
2020-12-19 16:56:12 +0100

    716A problem set solution -codeforces-

--- CrazyComputer__716A.cpp
+++ CrazyComputer__716A.cpp
@@ -1,4 +1,17 @@
-//
-// Created by Mohammad  Boustta on 12/19/20.
-//
-
+#include <iostream>
+using namespace std;
+int main(){
+    int n,c, in, ans{},p;
+    cin >> n >> c >> p;
+    int CC = c;
+    for (int i = 0; i < n-1; ++i) {
+        cin >> in;
+        if (in-p <= c)
+            ans++;
+        else if (in-p > c) { ans = 0; CC = c;}
+        CC+=c;
+        p = in;
+    }
+    cout << ans+1 << endl;
+    return 0;
+}
