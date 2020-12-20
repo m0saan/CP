8f8a64bec220416cf04a0510117dc415aa75faf2
MoBoustta <moboustta6@gmail.com>
2020-12-20 13:02:48 +0100

    677A problem set solution -codeforces-

--- VanyaAndFence__677A.cpp
+++ VanyaAndFence__677A.cpp
@@ -1,4 +1,14 @@
-//
-// Created by Mohammad  Boustta on 12/20/20.
-//
-
+#include <iostream>
+using namespace std;
+int main(){
+    int n, h, in;
+    long long count{};
+    cin >> n >> h;
+    for (int i = 0; i < n; ++i) {
+        cin >> in;
+        if (in > h) count += 2;
+        else count += 1;
+    }
+    cout << count << endl;
+    return 0;
+}
