876ce144a5f15010e6190a147563babb6de7d209
MoBoustta <moboustta6@gmail.com>
2020-12-20 13:02:30 +0100

    672A problem set solution -codeforces-

--- SummerCamp__672A.cpp
+++ SummerCamp__672A.cpp
@@ -1,4 +1,13 @@
-//
-// Created by Mohammad  Boustta on 12/20/20.
-//
-
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    if (n <= 9) {cout << n << endl; return 0;}
+    string S;
+    S.reserve(n);
+    for (int i = 0; i < n; ++i)
+        S += to_string(i);
+    cout << S[n]<< endl;
+    return 0;
+}
