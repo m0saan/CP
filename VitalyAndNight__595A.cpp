7daf906a58791e53be44322d8164ae209e9fe1c7
MoBoustta <moboustta6@gmail.com>
2020-12-21 19:58:50 +0100

    595A problem set solution -codeforces-

--- VitalyAndNight__595A.cpp
+++ VitalyAndNight__595A.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-
+    int n,m, count{},a,b, ans{};
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> a >> b;
+            if (a==1 || b==1) ans++;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
