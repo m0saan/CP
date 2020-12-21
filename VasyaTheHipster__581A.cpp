816e9c9254d9d89611a5aa61db215b51a1bfea5c
MoBoustta <moboustta6@gmail.com>
2020-12-21 19:59:33 +0100

    581A problem set solution -codeforces-

--- VasyaTheHipster__581A.cpp
+++ VasyaTheHipster__581A.cpp
@@ -1,4 +1,12 @@
-//
-// Created by Mohammad  Boustta on 12/21/20.
-//
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main(){
+    int a,b;
+    cin >> a >> b;
+    int diff = min(a,b);
+    cout << diff << ' ';
+    cout << (max(a,b) - diff)/2 << endl;
+    return 0;
+}
