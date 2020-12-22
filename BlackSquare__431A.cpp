68fb3b5da9fc8f51de9664518cfc2a44211464e5
MoBoustta <moboustta6@gmail.com>
2020-12-22 17:19:43 +0100

    431A problem set solution -codeforces-

--- BlackSquare__431A.cpp
+++ BlackSquare__431A.cpp
@@ -1,4 +1,14 @@
-//
-// Created by Mohammad  Boustta on 12/22/20.
-//
-
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main(){
+    int total{};
+    vector<int> V(4);
+    string S;
+    for (int i{}; i<4; ++i) cin >> V[i];
+    cin >> S;
+    for (char &c : S) total += V[c-48-1];
+    cout << total << endl;
+    return 0;
+}
