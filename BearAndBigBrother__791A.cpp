9bdaeab9e61cd9da29fbc801652e83925eec7755
MoBoustta <moboustta6@gmail.com>
2020-12-23 11:49:30 +0000

    791A problem set solution -codeforces-

--- BearAndBigBrother__791A.cpp
+++ BearAndBigBrother__791A.cpp
@@ -1,4 +1,12 @@
-//
-// Created by Mohammad  Boustta on 12/23/20.
-//
+#include <iostream>
+using namespace std;
 
+int main(){
+    int a,b;
+    cin >> a >> b;
+    for (int i = 1; ; ++i) {
+        a *= 3;
+        b *= 2;
+        if (a>b)  {cout << i << endl; return 0;}
+    }
+}
