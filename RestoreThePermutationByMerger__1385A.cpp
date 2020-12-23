1775653c8e548be1a72f58f003b177fd39731398
MoBoustta <moboustta6@gmail.com>
2020-12-23 19:03:28 +0100

    1385A problem set solution -codeforces-

--- RestoreThePermutationByMerger__1385A.cpp
+++ RestoreThePermutationByMerger__1385A.cpp
@@ -11,15 +11,16 @@ int main(){
 	for (int i=0; i<t; i++){
 		cin >> n;
 		vector<int> v;
-		for (int j=0; j<n; ++j){ 
+		for (int j=0; j<n*2; ++j){
 			cin >> in;
-			if (!set.count(in)) v.push_back(in);
-			else set.insert(in);
+            if (set.count(in)) continue;
+			set.insert(in);
+            v.push_back(in);
 		}
 		for (int &e : v)
 			cout << e << ' ';
 		cout << endl;
+		set.clear();
 	}
-	cout << "------------------------------------" << endl;
 	return 0;
 }
