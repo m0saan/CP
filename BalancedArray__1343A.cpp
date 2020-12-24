5e55be083225cd1bf9c0324f7667ea0703b36421
MoBoustta <moboustta6@gmail.com>
2020-12-24 16:48:52 +0100

    1343A problem set solution -codeforces-

--- BalancedArray__1343A.cpp
+++ BalancedArray__1343A.cpp
@@ -1,4 +1,28 @@
-//
-// Created by Mohammad  Boustta on 12/23/20.
-//
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int main() {
+    int t, in;
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+        cin >> in;
+        if (in % 4 != 0) cout << "NO" << endl;
+        else {
+            cout << "YES" << endl;
+            vector<int> v;
+            for (int j = 1; j <= in / 2; ++j)
+                v.push_back(2 * j);
+            v.push_back(1);
+            for (int j = 1; j < in/2; j+=1)
+                v.push_back(2 * j + 1);
+            v[v.size()-1] += in/2;
+            for (int &e : v)
+                cout << e << ' ';
+            cout << endl;
+            v.clear();
+        }
+    }
+    return 0;
+}
