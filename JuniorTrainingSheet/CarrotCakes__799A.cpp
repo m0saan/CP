0a48451f1c7a36f62e7d1cff5dacc70105cc274e
MoBoustta <moboustta6@gmail.com>
2020-12-27 16:10:23 +0100

    799A solution: -> Junior Training Sheet A

--- JuniorTrainingSheet/CarrotCakes__799A.cpp
+++ JuniorTrainingSheet/CarrotCakes__799A.cpp
@@ -1,8 +1,10 @@
-#include <istream>
+#include <iostream>
 using namespace std;
 
 int main(){
 	int n{}, t{}, k{}, d{};
 	cin >> n >> t >>  k >> d; 
+	if (( ( d / t + 1 ) * k) >= n) cout << "NO" << endl;
+	else cout << "YES" << endl;
 	return 0;
 }
