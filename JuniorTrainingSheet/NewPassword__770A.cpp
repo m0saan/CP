e3b78af6cad95b127db52c1215dd6e10556b7e5b
MoBoustta <moboustta6@gmail.com>
2020-12-28 15:40:38 +0100

    707A solution: -> Junior Training Sheet A

--- JuniorTrainingSheet/NewPassword__770A.cpp
+++ JuniorTrainingSheet/NewPassword__770A.cpp
@@ -1,4 +1,14 @@
-//
-// Created by Mohammad  Boustta on 12/28/20.
-//
+#include <iostream>
+#include <set>
+using namespace std;
+int main(){
+    int n,k, count{};
+    cin >> n >> k;
 
+    char alpha[]= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    set<int> s;
+    for (int i = 0; count < n; ++i) {
+        cout << alpha[i%k]; ++count;
+    }
+    return 0;
+}
