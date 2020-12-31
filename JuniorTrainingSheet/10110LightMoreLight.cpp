587b8c168a551e8276ccb5a7a437f511a6735198
MoBoustta <moboustta6@gmail.com>
2020-12-31 11:53:15 +0100

    10110 UVA problem's solution: -> Junior Training Sheet A

--- JuniorTrainingSheet/10110LightMoreLight.cpp
+++ JuniorTrainingSheet/10110LightMoreLight.cpp
@@ -1,4 +1,18 @@
-//
-// Created by Mohammad  Boustta on 12/28/20.
-//
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
 
+int main(){
+    long long in;
+    vector<int> v;
+    while (cin >> in && in!=0){
+        for (int i = 1; i*i <= in; ++i)
+            if (in%i==0) { v.push_back(in/i); }
+        if (v.size() % 2 ==0) cout << "YES"<< endl;
+        else cout << "NO" << endl;
+        cout << v.size() << endl;
+        v.clear();
+    }
+    return 0;
+}
