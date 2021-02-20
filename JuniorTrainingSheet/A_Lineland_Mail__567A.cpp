206e257fe8dd7d1c70dcab53b38428f8261eb3eb
MoBoustta <moboustta6@gmail.com>
2021-02-20 18:08:30 +0100

    567_A codeforces

---
+++ JuniorTrainingSheet/A_Lineland_Mail__567A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+
+    // Fast input-output
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    
+    return 0;
+}
