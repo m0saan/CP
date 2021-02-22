7df2cbfb9cdd21f67b9109cea3cd0d5a899ad758
MoBoustta <moboustta6@gmail.com>
2021-02-22 17:03:32 +0100

    B_Hamster_Farm__939B Solution

--- USACO/Introductory_USACO_Bronze_Problems/B_Hamster_Farm__939B.cpp
+++ USACO/Introductory_USACO_Bronze_Problems/B_Hamster_Farm__939B.cpp
@@ -10,15 +10,15 @@ using namespace std;
 
 int main(){
     long long N{}, K{}, in;
-    pair<int,int> pair1;
-    pair1.second = 999999999;
+    pair<long,long> pair1;
+    pair1.second = LLONG_MAX;
     cin >> N >> K;
-    vector<int> v(K,0);
+    vector<long long> v(K,0);
     for (int i = 0; i < K; ++i) {
         cin >> in;
         v[i] = in;
         if (N%in < pair1.second) { pair1.first =i+1;  pair1.second =  N%in; }
     }
-    cout << pair1.first << " " << (N-pair1.second) / v[pair1.first-1] << endl;
+    cout << (pair1.first==0 ? 1 : pair1.first) << " " << (N-pair1.second) / v[pair1.first-1] << endl;
     return 0;
 }
