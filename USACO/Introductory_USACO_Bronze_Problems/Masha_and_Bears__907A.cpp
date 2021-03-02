060118da35e65f416bd8a97542361009d954c8fc
MoBoustta <moboustta6@gmail.com>
2021-03-03 00:27:49 +0100

    Masha_and_Bears__907A solution

--- USACO/Introductory_USACO_Bronze_Problems/Masha_and_Bears__907A.cpp
+++ USACO/Introductory_USACO_Bronze_Problems/Masha_and_Bears__907A.cpp
@@ -1,22 +1,13 @@
-//
-// Created by Mohammad  Boustta on 2/25/21.
-//
-
-#include <iostream>
-#include <>
-
+#include<iostream>
+#include<algorithm>
 
 using namespace std;
 
 int main(){
-    int  V1, V2, V3, Vm;
-    cin >> V1 >> V2 >> V3 >> Vm;
-    if ((V1 < V2 || V2 < V3) || 2*V2) {
-        std::cout << -1 << endl;
-        return 0;
-    }
-
+    int v1,v2,v3,vm;
+    cin >> v1 >> v2 >> v3 >> vm;
+    if (v3>vm*2 || vm>v3*2 || vm >= v2)
+        cout << -1 << endl;
+    else cout << v1*2 << endl << v2*2 << endl << max(v3,vm) << endl;
     return 0;
 }
-
-// each bear likes its car, so size of i-th car is no more than 2·Vi;
