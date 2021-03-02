135de0a6b08b331497078ec98f3c83976b9d6cfb
MoBoustta <moboustta6@gmail.com>
2021-03-03 00:29:41 +0100

    Masha_and_Bears__907A solution

--- USACO/Introductory_USACO_Bronze_Problems/Masha_and_Bears__907A.cpp
+++ USACO/Introductory_USACO_Bronze_Problems/Masha_and_Bears__907A.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
     int v1,v2,v3,vm;
     cin >> v1 >> v2 >> v3 >> vm;
-    if (v3>vm*2 || vm>v3*2 || vm >= v2)
+    if (v3>vm*2 || vm>v3*2 || vm >= v2 || vm >= v1)
         cout << -1 << endl;
     else cout << v1*2 << endl << v2*2 << endl << max(v3,vm) << endl;
     return 0;
